9b8b66574caadbcc6e696cbf6852fadde5b75ecd
gustavbarbosaa <gustavoaraujohab@gmail.com>
2023-12-12 23:36:25 -0300

    questao 4 e questao 5

--- src/main/java/connection/ConnectionPostgreSQL.java
+++ src/main/java/connection/ConnectionPostgreSQL.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionPostgreSQL {
     public static final String URL =  "jdbc:postgresql://localhost:5432/crud_jdbc";
     public static final String USER =  "postgres";
-    public static final String PASSWORD =  "db123";
+    public static final String PASSWORD =  "guga1957";
 
     public static Connection getConnection(){
         Connection connection = null;
